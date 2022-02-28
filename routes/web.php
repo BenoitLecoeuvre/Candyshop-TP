b81dcdb71840d757e0b30fe6351b6afc7ce29688
Benoît Lecoeuvre <benoit_lecoeuvre@hotmail.fr>
2022-02-28 09:58:02 +0100

    Création controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/produits', [ProductController::class, 'index']);
